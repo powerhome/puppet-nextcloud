6311389bd1bd60a2878d433ec4fb1abe55040802
David Taylor <djtaylor13@gmail.com>
2017-03-15 13:51:27 -0400

    Use the correct path for chown

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,7 +15,7 @@ class nextcloud::install {
 
   # Set ownership on the docroot
   exec { 'nextcloud_docroot':
-    command => "/usr/sbin/chown -R ${::nextcloud::www_user}:${::nextcloud::www_group} ${::nextcloud::docroot}",
+    command => "/bin/chown -R ${::nextcloud::www_user}:${::nextcloud::www_group} ${::nextcloud::docroot}",
     unless  => "/usr/bin/test ! -z \"$(/bin/ls -ltrhad ${::nextcloud::docroot} | /bin/grep '${::nextcloud::www_user} ${::nextcloud::www_group}')\"",
   }
 }
