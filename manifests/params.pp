dab0e88e6bafee48a4802cf55e95e2670d9ebc4d
David Taylor <djtaylor13@gmail.com>
2017-03-16 14:31:10 -0400

    Use an alternate PPA key

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,6 +10,6 @@ class nextcloud::params {
   $config_file        = "${docroot}/config/config.php"
   $permissions_script = "/var/www/nextcloud_permissions.sh"
   $php_ppa            = 'ppa:ondrej/php'
-  $php_ppa_key        = '30B933D80FCE3D981A2D38FB0C99B70EF4FCBB07'
+  $php_ppa_key        = '6A1AAD7AE5B401C6E259A7B257067BAA1314C7FC'
   $php_version        = '7.0'
 }
