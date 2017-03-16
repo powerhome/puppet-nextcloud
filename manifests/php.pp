8dbaa9f41564691b357e83afe746dfc07cf716bf
David Taylor <djtaylor13@gmail.com>
2017-03-16 14:03:28 -0400

    Install the PPA key manually

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,6 +1,9 @@
 class nextcloud::php {
 
   apt::ppa { 'ppa:ondrej/php': }
+  apt::key { 'ppa:ondrej/php':
+    key => '30B933D80FCE3D981A2D38FB0C99B70EF4FCBB07',
+  }
 
   class { '::php::globals':
     php_version => '7.0',
