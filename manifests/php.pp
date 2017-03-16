a2cb71bcd6673d138ab4ad07bff46c8e5806d6d3
David Taylor <djtaylor13@gmail.com>
2017-03-16 14:00:10 -0400

    Dont manage repos and try to install ourself

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,10 +1,13 @@
 class nextcloud::php {
+
+  apt::ppa { 'ppa:ondrej/php': }
+
   class { '::php::globals':
     php_version => '7.0',
     config_root => '/etc/php/7.0',
   }->
   class { '::php':
-    manage_repos => true,
+    manage_repos => false,
     extensions   => {
         mysql    => {
           so_name => 'mysqli',
