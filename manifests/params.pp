bd7ddd81bac7b55cdd94793f7a029a2f1163ecaf
David Taylor <djtaylor13@gmail.com>
2017-03-17 14:32:48 -0400

    Re-enable core apache modules

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,5 +12,22 @@ class nextcloud::params {
   $php_version          = '7.0'
   $php_ppa              = 'ppa:ondrej/php'
   $php_ppa_key          = '14AA40EC0831756756D7F66C4F4EA0AAE5267A6C'
-  $apache_enabled_mods  = ['ssl', 'headers', 'rewrite']
+  $apache_enabled_mods  = [
+    'ssl',
+    'headers',
+    'rewrite',
+    'access_compat',
+    'auth_basic',
+    'authn_core',
+    'authn_file',
+    'authz_groupfile',
+    'authz_user',
+    'autoindex',
+    'cgi',
+    'deflate',
+    'dir',
+    'env',
+    'negotiation',
+    'reqtimeout'
+  ]
 }
