5b2b3a19f39e576d57ffa101b9448031a17a9cca
David Taylor <djtaylor13@gmail.com>
2017-03-17 15:55:07 -0400

    Try to fix allowoverride and require statements

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -55,7 +55,8 @@ class nextcloud::vhost {
       {
         path            => $::nextcloud::docroot,
         options         => ['+FollowSymLinks'],
-        allow_overide   => 'All',
+        allow_overide   => ['All'],
+        require         => undef,
         directoryindex  => 'index.php index.html index.cgi index.pl index.php index.xhtml index.htm',
         setenv          => [
           "HOME ${::nextcloud::docroot}",
