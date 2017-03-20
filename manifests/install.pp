8e006cd1490c1f22f22c6ecbfc138b5395e2889e
David Taylor <djtaylor13@gmail.com>
2017-03-20 16:12:58 -0400

    Create the nextcloud log file and logrotate rule

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,25 @@ class nextcloud::install {
     ensure => present,
   }
 
+  # Create log file
+  file { '/var/log/nextcloud.log':
+    ensure => file,
+    mode   => '0644',
+    owner  => $::nextcloud::www_user,
+    group  => $::nextcloud::www_group,
+  }
+
+  # Log rotation
+  logrotate::rule { 'nextcloud':
+    path         => '/var/log/nextcloud.log',
+    rotate       => 5,
+    rotate_every => 'day',
+    create       => true,
+    create_mode  => '0644',
+    create_owner => $::nextcloud::www_user,
+    create_group => $::nextcloud::www_group,
+  }
+
   # Deploy command line tool wrapper
   file { '/usr/bin/occ':
     ensure  => file,
