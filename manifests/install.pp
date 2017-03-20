350fe67e62f07c4401e68a591ba3849e6c94e9df
David Taylor <djtaylor13@gmail.com>
2017-03-20 11:33:41 -0400

    Removed duped code

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,5 @@
 class nextcloud::install {
 
-  # Download and install a specific tagged release
-  vcsrepo { $::nextcloud::docroot:
-    ensure   => present,
-    provider => git,
-    source   => $::nextcloud::repo_url,
-    revision => "v${::nextcloud::repo_version}",
-  }
-
   # Install Apache
   class { 'apache':
     default_vhost => false,
