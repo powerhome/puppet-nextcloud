7fe80c21854684e9e397cb84593272e2c12831e8
David Taylor <djtaylor13@gmail.com>
2017-03-20 16:04:21 -0400

    Try to get installation working

--- manifests/install.pp
+++ manifests/install.pp
@@ -33,7 +33,8 @@ class nextcloud::install {
   # Run installation
   exec { 'nextcloud_install':
     command => "/bin/bash ${::nextcloud::install_script}",
-    unless  => '/usr/bin/test -z "$(/usr/bin/occ status | /bin/grep "installed: false")"',
+    unless  => "/bin/bash ${::nextcloud::check_install_script}",
+    unless  => '/usr/bin/test ! -z "$(/usr/bin/occ status | /bin/grep "installed: true")"',
     require => [File['/usr/bin/occ'], File[$::nextcloud::install_script]],
   }
 
