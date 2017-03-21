ee5e2ad2cf0c57cb6e29d7852a206cbd154492f9
David Taylor <djtaylor13@gmail.com>
2017-03-21 13:30:48 -0400

    keep unless and remove require

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -1,14 +1,14 @@
 class nextcloud::ldap {
   exec { 'ldap_config_init':
     command => '/usr/bin/occ ldap:create-empty-config',
+    unless  => '/usr/bin/test ! -z "$(/usr/bin/occ ldap:show-config)"',
   }
 
   # Setup LDAP configuration
   $::nextcloud::ldap_config.each |$key, $value| {
     nextcloud_ldap_config { $key:
-      ensure  => present,
-      value   => $value,
-      require => Exec['ldap_config_init'],
+      ensure => present,
+      value  => $value,
     }
   }
 }
