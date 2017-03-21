ef7f8135784ff8d612ca6d356219c0dbecdc8f0a
David Taylor <djtaylor13@gmail.com>
2017-03-21 12:59:36 -0400

    Better debug output

--- lib/puppet/provider/nextcloud_ldap_config/occ.rb
+++ lib/puppet/provider/nextcloud_ldap_config/occ.rb
@@ -15,11 +15,11 @@ Puppet::Type.type(:nextcloud_ldap_config).provide(:occ) do
     # Process each LDAP configuration definition
     config.each do |line|
       if line =~ /^\| [a-zA-Z]/
-        notice("LDAP config line: #{line}")
         key, value = /^\|[ ]([^ ]*)[ ]*\|[ ]*([^ \|]*).*$/.match(line).captures
 
         # Ignore configuration title
         if key != 'Configuration'
+          notice("Discovered LDAP configuration: #{key} = #{value}")
 
           # New instance
           instances << new(
