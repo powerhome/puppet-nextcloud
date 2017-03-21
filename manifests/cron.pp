2fbf338c12c3a9577aba1c533a1e591fdd1ed018
David Taylor <djtaylor13@gmail.com>
2017-03-21 10:15:32 -0400

    escape single quotes

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -10,7 +10,7 @@ class nextcloud::cron {
   # Make sure installation is using cron
   exec { 'nextcloud_cron':
     command => '/usr/bin/occ background:cron',
-    unless  => '/usr/bin/test ! -z "$(/usr/bin/occ config:list | grep '"backgroundjobs_mode": "cron"')"',
+    unless  => '/usr/bin/test ! -z "$(/usr/bin/occ config:list | grep \'"backgroundjobs_mode": "cron"\')"',
     require => Cron['nextcloud'],
   }
 }
