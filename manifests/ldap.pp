6c92abf9b5863d47a7a7ba41b9fd6f3319ef9ac5
David Taylor <djtaylor13@gmail.com>
2017-03-21 13:26:53 -0400

    remove unless to make sure ldap config provider gets run

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -1,7 +1,6 @@
 class nextcloud::ldap {
   exec { 'ldap_config_init':
     command => '/usr/bin/occ ldap:create-empty-config',
-    unless  => '/usr/bin/test ! -z "$(/usr/bin/occ ldap:show-config)"',
   }
 
   # Setup LDAP configuration
