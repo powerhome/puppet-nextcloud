7216a51c1e42bc609463aae14f08285f5aea4b4d
David Taylor <djtaylor13@gmail.com>
2017-04-06 09:36:27 -0400

    Cron job to periodically clean up bruteforce attempts table

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -13,4 +13,15 @@ class nextcloud::cron {
     unless  => '/usr/bin/test ! -z "$(/usr/bin/occ config:list | grep \'"backgroundjobs_mode": "cron"\')"',
     require => Cron['nextcloud'],
   }
+
+  # Cron job for cleaning up bruteforce attempts table
+  if ($::role_instance == '1') {
+
+    # Cron job for cleaning up bruteforce attempts table
+    cron { 'nextcloud_clean_bruteforce':
+      command => "/usr/bin/mysql -u ${::nextcloud::db_user} -p${::nextcloud::db_pass} -h ${::nextcloud::db_host} -e \"delete from ${::nextcloud::db_name}.oc_bruteforce_attempts\"",
+      user    => 'root',
+      hour    => 1,
+    }
+  }
 }
