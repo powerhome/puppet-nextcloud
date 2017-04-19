4485f14533e4490f8f8675ee61fcbcb551d6a19c
David Taylor <djtaylor13@gmail.com>
2017-04-19 09:55:25 -0400

    Disable cron job due to table locking issues

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,11 +1,11 @@
 class nextcloud::cron {
 
   # Nextcloud cron jobs
-  cron { 'nextcloud':
-    command => "/usr/bin/php -f ${::nextcloud::docroot}/cron.php",
-    user    => $::nextcloud::www_user,
-    minute  => '*/15',
-  }
+  #cron { 'nextcloud':
+  #  command => "/usr/bin/php -f ${::nextcloud::docroot}/cron.php",
+  #  user    => $::nextcloud::www_user,
+  #  minute  => '*/15',
+  #}
 
   # Make sure installation is using cron
   exec { 'nextcloud_cron':
