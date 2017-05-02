ea619916b988f9ad7d74d10841056be6acab1154
David Taylor <djtaylor13@gmail.com>
2017-05-02 09:52:04 -0400

    Upgrade to newest NC release, re-enable cron jobs

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,17 +1,17 @@
 class nextcloud::cron {
 
   # Nextcloud cron jobs
-  #cron { 'nextcloud':
-  #  command => "/usr/bin/php -f ${::nextcloud::docroot}/cron.php",
-  #  user    => $::nextcloud::www_user,
-  #  minute  => '*/15',
-  #}
+  cron { 'nextcloud':
+    command => "/usr/bin/php -f ${::nextcloud::docroot}/cron.php",
+    user    => $::nextcloud::www_user,
+    minute  => '*/15',
+  }
 
   # Make sure installation is using cron
   exec { 'nextcloud_cron':
     command => '/usr/bin/occ background:cron',
     unless  => '/usr/bin/test ! -z "$(/usr/bin/occ config:list | grep \'"backgroundjobs_mode": "cron"\')"',
-    #require => Cron['nextcloud'],
+    require => Cron['nextcloud'],
   }
 
   # Cron job for cleaning up bruteforce attempts table
