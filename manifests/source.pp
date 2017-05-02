c3e52a55fdd3d7dd5d804874f91c76fd05af5963
David Taylor <djtaylor13@gmail.com>
2017-05-02 10:12:44 -0400

    Use the archive provider instead of vcsrepo

--- manifests/source.pp
+++ manifests/source.pp
@@ -1,10 +1,17 @@
 class nextcloud::source {
+  $install_file = "nextcloud-${::nextcloud::repo_version}.zip"
+  $source       = "https://download.nextcloud.com/server/releases/${install_file}"
+  $sha256sum    = 'd23095a517cbd547fb29bccde71e6d9faaa394b71fa89329f42cfa28b6aaffd3'
 
-  # Download and install a specific tagged release
-  vcsrepo { $::nextcloud::docroot:
-    ensure   => present,
-    provider => git,
-    source   => $::nextcloud::repo_url,
-    revision => "v${::nextcloud::repo_version}",
+  # Download and install the specified version
+  archive { $install_file:
+    ensure        => present,
+    name          => "/var/tmp/${install_file}",
+    source        => $source,
+    checksum      => $sha256sum,
+    checksum_type => 'sha256',
+    extract       => true,
+    extract_path  => $::nextcloud::docroot,
+    creates       => "${::nextcloud::docroot}/nextcloud",
   }
 }
